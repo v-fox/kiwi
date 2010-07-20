d5ff176baf20b92d278aed8b2d56ac9a1ce98ee2
Marcus Schäfer <ms@novell.com>
2010-07-20 17:38:33 +0200

    - added main::isize function to replace the use of   the -s operator. This is done because of SUSE Studio   which maps a file to a block special under certain   circumstances which causes -s to not work anymore

--- modules/KIWIImage.pm
+++ modules/KIWIImage.pm
@@ -3106,7 +3106,8 @@ sub writeImageConfig {
 					$targetPartitionNext = $targetPartition + 1;
 				}
 				if ($href -> {size} eq "image") {
-					print FD int(((-s "$this->{imageDest}/$name")/1024/1024)+1);
+					my $size = main::isize ("$this->{imageDest}/$name");
+					print FD int (($size/1024/1024)+1);
 				} else {
 					print FD $href -> {size};
 				}
@@ -4084,7 +4085,7 @@ sub buildMD5Sum {
 	# Create image md5sum
 	#------------------------------------------
 	$kiwi -> info ("Creating image MD5 sum...");
-	my $size = -s "$this->{imageDest}/$name";
+	my $size = main::isize ("$this->{imageDest}/$name");
 	my $primes = qxx ("factor $size"); $primes =~ s/^.*: //;
 	my $blocksize = 1;
 	for my $factor (split /\s/,$primes) {
@@ -4182,7 +4183,7 @@ sub compressImage {
 			return undef;
 		}
 		my $line = <FD>; close FD; chomp $line;
-		my $size = -s "$this->{imageDest}/$name.gz";
+		my $size = main::isize ("$this->{imageDest}/$name.gz");
 		my $primes = qxx ("factor $size"); $primes =~ s/^.*: //;
 		my $blocksize = 1;
 		for my $factor (split /\s/,$primes) {
