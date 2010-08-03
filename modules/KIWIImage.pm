cb182e51db79f4585fb3a56cfa64d0e296498bff
Marcus Schäfer <ms@novell.com>
2010-08-03 11:27:45 +0200

    - if a fixed xml size was set for a filesystem image we use if   even if kiwi's calculation says it's too small. This allows users   to create the image with less or no free space but also could   cause the image to fail because of no space left on device.   We don't use the fixed xml size if it's too small for a virtual   disk based image though because this could cause the image to   be created successfully but then failed on boot which is a bad   user experience (bnc #627031)

--- modules/KIWIImage.pm
+++ modules/KIWIImage.pm
@@ -4262,6 +4262,16 @@ sub getSize {
 			}
 			# convert MB to Byte
 			$xmlsize = $value * 1048576;
+			# check the size value with what kiwi thinks is the minimum
+			if ($xmlsize < $minsize) {
+				$kiwi -> warning (
+					"--> given xml size might be too small, using it anyhow !\n"
+				);
+				$kiwi -> warning (
+					"--> min size changed from $minsize to $xmlsize bytes\n"
+				);
+				$minsize = $xmlsize;
+			}
 		}
 	}
 	#==========================================
