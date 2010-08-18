0fd051fbb65bc85ca5994045e33cf9944801c2d8
Marcus Schäfer <ms@novell.com>
2010-08-18 11:11:44 +0200

    - disable filesystem check for kiwi managed partitions   by resetting the mount counter using using tune2fs   (bnc #627021)

--- modules/KIWILinuxRC.sh
+++ modules/KIWILinuxRC.sh
@@ -3908,8 +3908,40 @@ function mountSystem {
 		mountSystemStandard "$mountDevice"
 		retval=$?
 	fi
+	#======================================
+	# reset mount counter
+	#--------------------------------------
+	resetMountCounter
 	IFS=$OLDIFS
 	return $retval
+}
+#======================================
+# resetMountCounter
+#--------------------------------------
+function resetMountCounter {
+	local curtype=$FSTYPE
+	local command
+	for device in \
+		$imageRootDevice $imageBootDevice \
+		$imageRecoveryDevice $imageHomeDevice
+	do
+		if [ ! -e $device ];then
+			continue
+		fi
+		probeFileSystem $device
+		if [ "$FSTYPE" = "ext2" ];then
+			command="tune2fs -c -1 -i 0"
+		elif [ "$FSTYPE" = "ext3" ];then
+			command="tune2fs -c -1 -i 0"
+		elif [ "$FSTYPE" = "ext4" ];then
+			command="tune2fs -c -1 -i 0"
+		else
+			# nothing to do here...
+			continue
+		fi
+		eval $command $device 1>&2
+	done
+	FSTYPE=$curtype
 }
 #======================================
 # cleanDirectory
