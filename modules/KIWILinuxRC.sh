9c1cb1b412742d14d6d4c1033ffc5988442c298f
Marcus Schäfer <ms@novell.com>
2010-09-16 15:18:24 +0200

    - removed umount -a call to prevent a read-only remount (bnc #635159)

--- modules/KIWILinuxRC.sh
+++ modules/KIWILinuxRC.sh
@@ -4592,10 +4592,6 @@ function cleanImage {
 		return
 	fi
 	#======================================
-	# umount non busy fstab listed entries
-	#--------------------------------------
-	umount -a &>/dev/null
-	#======================================
 	# umount LVM root parts lazy
 	#--------------------------------------
 	if [ "$haveLVM" = "yes" ]; then
