a3cc45c959cd8d1c963f244dd658b76470c45710
Marcus Schäfer <ms@novell.com>
2010-11-02 10:07:31 +0100

    - keeping old boot files in /boot and trying to copy   in newly generated initrd + kernel during image startup   could result in not enough space on /boot partition for LVM   enabled builds. This patch fixes that by removing the old   kernel + initrd, before copying in the new one.

--- modules/KIWILinuxRC.sh
+++ modules/KIWILinuxRC.sh
@@ -5926,6 +5926,7 @@ function setupBootPartition {
 	fi
 	mkdir -p /mnt/$mpoint
 	mount $imageBootDevice /mnt/$mpoint
+	rm -fr /mnt/$mpoint/*
 	cp -a /mnt/boot /mnt/$mpoint
 	if [ -e /boot.tgz ];then
 		tar -xf /boot.tgz -C /mnt/$mpoint
