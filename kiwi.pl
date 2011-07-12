3a179c05dc1c3573fc817e95888c44041027237f
Marcus Schäfer <ms@novell.com>
2011-07-12 11:11:36 +0200

    - added documentation about pxe RAID feature (bnc #704707)

--- kiwi.pl
+++ kiwi.pl
@@ -45,7 +45,7 @@ use KIWITest;
 #============================================
 # Globals (Version)
 #--------------------------------------------
-our $Version       = "3.74.1";
+our $Version       = "3.74.2";
 our $Publisher     = "SUSE LINUX Products GmbH";
 our $Preparer      = "KIWI - http://kiwi.berlios.de";
 our $openSUSE      = "http://download.opensuse.org";
