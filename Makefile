0fd2a0b666accec65e187ed5d935ddbd789634ec
Marcus Schäfer <ms@suse.com>
2015-09-08 16:22:11 +0200

    Use python generateDS to create parser
    http://pythonhosted.org/generateDS is a great project to simplify
    the creation of parser classed from an XML schema. It creates
    getter and setter methods for us automatically while we can
    continue to maintain the schema

--- Makefile
+++ Makefile
@@ -1,4 +1,13 @@
-all:
+all: xml_parser
+
+xml_parser: schema/KIWISchema.xsd
+	# XML parser code is auto generated from schema using generateDS
+	# http://pythonhosted.org/generateDS
+	generateDS.py --external-encoding='utf-8' \
+		-o kiwi/xml.py schema/KIWISchema.xsd
+
+schema/KIWISchema.xsd: schema/KIWISchema.rnc
+	trang -I rnc -O xsd schema/KIWISchema.rnc schema/KIWISchema.xsd
 
 clean:
 	${MAKE} -C schema clean
---
+++ kiwi/kiwi.py
@@ -0,0 +1,28 @@
+# Copyright (c) 2015 SUSE Linux GmbH.  All rights reserved.
+#
+# This file is part of kiwi.
+#
+# kiwi is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# kiwi is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with kiwi.  If not, see <http://www.gnu.org/licenses/>
+
+import xml
+
+def main():
+    """
+        kiwi - main entry
+    """
+    image_xml = xml.parse('/home/ms/config.xml', True)
+
+    print image_xml.get_name()
+
+    print image_xml.get_preferences()[0].get_type()[0].get_image()
