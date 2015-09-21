df942d527f9e589ada3b537c2d2df8e8cb1399c7
Marcus Schäfer <ms@suse.com>
2015-09-21 17:06:31 +0200

    Add root filesystem label to images
    In order to allow devicepersistency="by-label" also for the
    root filesystem, the setupFilesystem method has been extended
    for all supported filesystems to add a root label too. The
    default name for the root label is 'ROOT'. This addresses
    bnc#946236

--- modules/KIWIBoot.pm
+++ modules/KIWIBoot.pm
@@ -1317,7 +1317,8 @@ sub setupInstallStick {
             #==========================================
             # build root filesystem
             #------------------------------------------
-            if (! $this -> setupFilesystem ('ext3',$root,"install-root")) {
+            if (! $this -> setupFilesystem ('ext3',$root,"install-root","ROOT")
+            ) {
                 $this -> cleanStack ();
                 return;
             }
@@ -2596,7 +2597,7 @@ sub setupBootDisk {
         #==========================================
         # Create fs on system image partition
         #------------------------------------------
-        if (! $this -> setupFilesystem ($FSTypeRO,$root,"root")) {
+        if (! $this -> setupFilesystem ($FSTypeRO,$root,"root","ROOT")) {
             return;
         }
         #==========================================
@@ -2655,7 +2656,9 @@ sub setupBootDisk {
                         $this -> cleanStack ();
                         return;
                     }
-                    if (! $this -> setupFilesystem ($FSTypeRO,$device,$pname)) {
+                    if (! $this -> setupFilesystem (
+                        $FSTypeRO,$device,$pname,$pname
+                    )) {
                         $this -> cleanStack ();
                         return;
                     }
@@ -7208,6 +7211,9 @@ sub setupFilesystem {
             $kiwi -> info ("Creating reiserfs $name filesystem");
             my $createArgs = $fsOpts -> getOptionsStrReiser();
             $createArgs .= " -f";
+            if ($bootp) {
+                $createArgs.= " -l '".$bootp."'";
+            }
             $status = KIWIQX::qxx (
                 "/sbin/mkreiserfs $createArgs $device 2>&1"
             );
@@ -7217,6 +7223,9 @@ sub setupFilesystem {
         /^btrfs/        && do {
             $kiwi -> info ("Creating btrfs $name filesystem");
             my $createArgs = $fsOpts -> getOptionsStrBtrfs();
+            if ($bootp) {
+                $createArgs.= " -L '".$bootp."'";
+            }
             $status = KIWIQX::qxx (
                 "/sbin/mkfs.btrfs $createArgs $device 2>&1"
             );
@@ -7226,6 +7235,9 @@ sub setupFilesystem {
         /^xfs/          && do {
             $kiwi -> info ("Creating xfs $name filesystem");
             my $createArgs = $fsOpts -> getOptionsStrXFS();
+            if ($bootp) {
+                $createArgs.= " -L '".$bootp."'";
+            }
             $status = KIWIQX::qxx (
                 "/sbin/mkfs.xfs $createArgs $device 2>&1"
             );
