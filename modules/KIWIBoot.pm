dbb10ffb53a952569a2bcd48d8d0512c8e61d34e
Marcus Schäfer <ms@suse.com>
2015-10-19 12:35:33 +0200

    Set noudevsync only if udevd is not running
    LVM commands support the --noudevsync option which should be
    used only if udevd is not running (bnc#950735)

--- modules/KIWIBoot.pm
+++ modules/KIWIBoot.pm
@@ -6919,13 +6919,20 @@ sub setVolumeGroup {
     push @cStack,"vgchange -an $VGroup";
     $this->{cleanupStack} = \@cStack;
     $kiwi -> info ("--> Creating logical volumes\n");
+    my $lvm_opts = "";
+    $status = KIWIQX::qxx ("pidof systemd-udevd 2>&1");
+    $result = $? >> 8;
+    if ($result != 0) {
+        # udev daemon is not running, pass noudevsync to lvm commands
+        $lvm_opts = "--noudevsync";
+    }
     if (($syszip) || ($haveSplit)) {
         $status = KIWIQX::qxx (
-            "lvcreate --noudevsync -L $syszip -n LVComp $VGroup 2>&1"
+            "lvcreate $lvm_opts -L $syszip -n LVComp $VGroup 2>&1"
         );
         $result = $? >> 8;
         $status.= KIWIQX::qxx (
-            "lvcreate --noudevsync -l +100%FREE -n LVRoot $VGroup 2>&1"
+            "lvcreate $lvm_opts -l +100%FREE -n LVRoot $VGroup 2>&1"
         );
         $result+= $? >> 8;
         if ($result != 0) {
@@ -6963,7 +6970,7 @@ sub setVolumeGroup {
                     }
                     $ihash{$lvdev} = $inodes;
                     $status = KIWIQX::qxx (
-                        "lvcreate --noudevsync -L $lvsize -n $lvname $VGroup 2>&1"
+                        "lvcreate $lvm_opts -L $lvsize -n $lvname $VGroup 2>&1"
                     );
                     $result = $? >> 8;
                     if ($result != 0) {
@@ -6977,11 +6984,11 @@ sub setVolumeGroup {
             if (($lvmparts{'@root'}) && ($lvmparts{'@root'}->[3])) {
                 my $rootsize = $lvmparts{'@root'}->[3];
                 $status = KIWIQX::qxx (
-                    "lvcreate --noudevsync -L $rootsize -n LVRoot $VGroup 2>&1"
+                    "lvcreate $lvm_opts -L $rootsize -n LVRoot $VGroup 2>&1"
                 );
             } else {
                 $status = KIWIQX::qxx (
-                    "lvcreate --noudevsync -l +100%FREE -n LVRoot $VGroup 2>&1"
+                    "lvcreate $lvm_opts -l +100%FREE -n LVRoot $VGroup 2>&1"
                 );
             }
             $result = $? >> 8;
